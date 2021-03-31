91c19c52c27652c17d9c408c2f2a12118ddaa849
namanjain7 <namanjain1568@gmail.com>
2021-03-31 18:38:08 +0530

    sorted array to BST and vim

--- vim_practice/p1.cpp
+++ vim_practice/p1.cpp
@@ -1,15 +1,10 @@
 #include<iostream>
-using namespace std;
-
-void merge(int arr[], int l, int r){
-	int n1 = m - l + 1;
-	int n2 = r - m;
+using namespace std; void merge(int arr[], int l, int r){ int n1 = m - l + 1; int n2 = r - m;
 
 	int L[n1], R[n2];
 
 	for(int i = 0; i < n1; i++)
-		L[i] = arr[l + i];
-	for(int j = 0; j < n2; j++)
+		L[i] = arr[l + i]; for(int j = 0; j < n2; j++)
 		R[j] = arr[m + 1 + j];
 
 	int i = 0;
