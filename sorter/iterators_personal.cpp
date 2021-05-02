9e99915c13f0399a19b76a2e28809aac9a3e5304
namanjain7 <namanjain1568@gmail.com>
2021-05-02 08:47:13 +0530

    insertion and deletion operations using vector iterators

--- sorter/iterators_personal.cpp
+++ sorter/iterators_personal.cpp
@@ -15,7 +15,23 @@ int main(){
     vector<int> v = {1, 4, 3, 67, 12, 0, 23, 4};
     sort(v.begin(), v.end());
     print_vector(v);
+    
     auto a = v.insert(v.begin() + 3, 2, 600);
+    print_vector(v);
+
+    //inserting element at begining in vectors using iterators
+    vector<int>::iterator i;
+    for(i = v.begin(); i != v.end(); ++i){
+        if(i == v.begin())  i = v.insert(i, 10000);
+        else continue;
+    }
+    //deletin element at end using vectors using iterators
+
+    for(i = v.begin(); i < v.end(); i++)
+        if(i == v.begin()) i = v.erase(i);
+    
+    i = v.erase(--v.end());
+
     print_vector(v);
     return 0;
 }
